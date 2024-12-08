ed36424676875d66ac85327814d9e86a6b69e949
Guido Schmutz <schmutz68@gmail.com>
2024-12-08 14:18:55 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -23,11 +23,13 @@ if [[ "$VERBOSE" == "yes" ]]; then
 fi
 
 if [[ -v NIFI2_URL && -n "$NIFI2_URL" ]]; then
+    # We have to wait for NiFi to become available so that the nifi.properties file is properly initialized!
+    wait4x http $NIFI2_URL/nifi-api --insecure-skip-tls-verify --timeout 400s
+
     /app/create-nifi-toolkit-properties.sh /opt/nifi/conf
 
     cli.sh session set nifi.props /opt/nifi/conf/nifi-toolkit.properties
 fi
-
 
 # run all the shell scripts in the init folder once
 source run-init
