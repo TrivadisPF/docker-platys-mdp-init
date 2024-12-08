4456fa9f9a7f0eb89042a3e3f314327da1fd27d8
Guido Schmutz <schmutz68@gmail.com>
2024-12-08 21:17:22 +0100

    add support for DELETE and PATCH to airflow-api.sh

--- airflow-api.sh
+++ airflow-api.sh
@@ -5,7 +5,7 @@ if [ $# -lt 5 ]
 then
   echo "Usage: airflow-api <airflowBaseUrl> <accessKeyId> <secretAccessKey> <method> <resource>"
   echo "       airflowBaseUrl: the base URL of the Vault server as http(s)://<host:port>"
-  echo "       method: POST | PUT | GET"
+  echo "       method: POST | PUT | GET | DELETE | PATCH"
   echo "       resource: the resource to invoke, do not prefix it with /"
   exit 1
 fi
@@ -18,13 +18,13 @@ method=$4
 resource=$5
 
 # Validate the HTTP method
-if [[ "$method" != "POST" && "$method" != "PUT" && "$method" != "GET" ]]; then
-  echo "Error: Invalid method. Allowed methods are POST, PUT, or GET."
+if [[ "$method" != "POST" && "$method" != "PUT" && "$method" != "DELETE" && "$method" != "GET" && "$method" != "PATCH"]]; then
+  echo "Error: Invalid method. Allowed methods are POST, PUT, DELETE, GET or PATCH."
   exit 1
 fi
 
 # Prepare the curl command dynamically based on the method
-if [ "$method" == "GET" ]; then
+if [ "$method" == "GET" ] || [ "$method" == "DELETE" ]; then
   curl -X GET --user "$username:$password" -H "Content-Type: application/json" --insecure -k $airflowBaseUrl/api/v1/$resource
 else
   dataFile=$6
